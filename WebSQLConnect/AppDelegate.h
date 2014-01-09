2f7da9cb1d34040b3fe59b20de5edf145fe57149
kevin thornton <kevint@kevins.mini.com>
2014-01-09 09:01:07 -0800

    Initial Commit

---
+++ WebSQLConnect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebSQLConnect
+//
+//  Created by kevin thornton on 1/9/14.
+//  Copyright (c) 2014 kevin thornton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
